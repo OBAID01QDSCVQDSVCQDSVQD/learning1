71200eae844107d6c5d69021386e706da4aeee08
obaid <obaidessafi@gmail.com>
2023-03-20 21:41:46 +0100

    CC
    Signed-off-by: obaid <obaidessafi@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.css";
+
+export default function App() {
+  return (
+  <section>
+      <section>
+        <h1 className="h11"> desidn is my life</h1>
+        <p>
+          {" "}
+          Grafic bbtt and web design is my favorit think in my life Grafic and web
+          design is my favorit think in my life Grafic and web design is my
+          favorit think in my life{" "}
+        </p>
+        <ul>
+          <li>facebookkk</li>
+          <li>instagram</li>
+          <li>linkedin</li>
+          <li>youtube</li>
+        </ul>
+      </section>
+  </section>
+  );
+}
